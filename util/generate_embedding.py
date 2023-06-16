b97d77975dc014e133da210ace3e84c0dbea3baf
Cole Gawin <colegawin@gmail.com>
2023-06-15 21:55:42 -0500

    🚑 Fix pathname to model

--- util/generate_embedding.py
+++ util/generate_embedding.py
@@ -4,7 +4,7 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-path_name = "../../util/model/multi-qa-MiniLM-L6-cos-v1"
+path_name = "model/multi-qa-MiniLM-L6-cos-v1"
 absolute_path = os.path.join(current_dir, path_name)
 
 model = SentenceTransformer(absolute_path, device="cpu")
